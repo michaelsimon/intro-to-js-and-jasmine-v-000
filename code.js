a158b8fe2dc4e3b7d655f95c1d02c17e61bc74cb
michaelsimon <michael.simon@gmail.com>
2016-07-05 21:28:39 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-'use strict';
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
